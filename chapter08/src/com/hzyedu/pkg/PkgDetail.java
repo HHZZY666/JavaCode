91f66a4b83c0b3eb663d5cc07db0e8d526989352
hhzzy666 <hzy050516@qq.com>
2025-03-30 18:42:49 +0800

    Java idea

---
+++ chapter08/src/com/hzyedu/pkg/PkgDetail.java
@@ -0,0 +1,13 @@
+package com.hzyedu.pkg;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PkgDetail {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arr[] = {0, 9, 10};
+        Arrays.sort(arr);
+    }
+}
