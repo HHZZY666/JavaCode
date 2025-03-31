8d15190b0d1d806661a4fc8b3029e0942f19f4b2
hhzzy666 <hzy050516@qq.com>
2025-03-31 19:52:02 +0800

    Java idea

---
+++ chapter08/src/com/hzyedu/super_/A.java
@@ -0,0 +1,24 @@
+package com.hzyedu.super_;
+
+public class A extends Super01{
+    //4 个属性
+    public int n1 = 100;
+    protected int n2 = 200;
+    int n3 = 300;
+    private int n4 = 400;
+    public A() {}
+    public A(String name) {}
+    public A(String name, int age) {}
+     public void cal() {
+     System.out.println("A 类的 cal() 方法...");
+     }
+    public void test100() {
+    }
+    protected void test200() {
+    }
+    void test300() {
+    }
+    private void test400() {
+    }
+}
+
---
+++ chapter08/src/com/hzyedu/super_/Base.java
@@ -0,0 +1,12 @@
+package com.hzyedu.super_;
+
+public class Base { //父类是 Object
+    public int n1 = 999;
+    public int age = 111;
+    public void cal() {
+        System.out.println("Base 类的 cal() 方法...");
+    }
+    public void eat() {
+        System.out.println("Base 类的 eat().....");
+    }
+}
