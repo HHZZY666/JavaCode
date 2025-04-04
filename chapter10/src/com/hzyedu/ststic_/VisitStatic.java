cd9a0c68175bfe45417751adc0270a096be2e589
HHZZY666 <hhzzy666@gmail.com>
2025-04-04 19:03:48 +0800

    Add files via upload

---
+++ chapter10/src/com/hzyedu/ststic_/VisitStatic.java
@@ -0,0 +1,20 @@
+package com.hzyedu.ststic_;
+
+public class VisitStatic {
+    public static void main(String[] args) {
+
+        //类名.变量名
+        //说明：类变量是随着类的加载而创建，所以即使没有创建对象实例也可以访问
+        System.out.println(A.name);
+        A a = new A();
+        //通过对象名.类对象名
+        System.out.println("a.name = " + a.name);
+    }
+}
+
+class A{
+    //类变量
+    //类变量的访问，必须遵守相关访问权限
+    public static String name = "韩顺平教育";
+
+}
