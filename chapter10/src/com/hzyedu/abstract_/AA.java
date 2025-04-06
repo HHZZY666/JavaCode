b52feeff25ae1ff6c7e138e9e7d4c6eb22912c52
HHZZY666 <hhzzy666@gmail.com>
2025-04-06 21:24:31 +0800

    Add files via upload

---
+++ chapter10/src/com/hzyedu/abstract_/AA.java
@@ -0,0 +1,10 @@
+package com.hzyedu.abstract_;
+
+public class AA extends Template{
+    public void job(){ //重写了Template的job方法
+        long num = 0;
+        for (int i = 1; i <= 999999999; i++) {
+            num += i;
+        }
+    }
+}
---
+++ chapter10/src/com/hzyedu/abstract_/TestTemplate.java
@@ -0,0 +1,11 @@
+package com.hzyedu.abstract_;
+
+public class TestTemplate {
+    public static void main(String[] args) {
+
+        AA aa = new AA();
+        aa.caculateTime();
+        BB bb = new BB();
+        bb.caculateTime();
+    }
+}
