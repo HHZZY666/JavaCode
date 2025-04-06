d1d4728b7566ae366032f552aae82a5a46423414
hhzzy666 <hzy050516@qq.com>
2025-04-06 20:02:50 +0800

    Java idea

---
+++ chapter10/src/com/hzyedu/abstract_/Abstract01.java
@@ -0,0 +1,27 @@
+package com.hzyedu.abstract_;
+
+public class Abstract01 {
+    public static void main(String[] args) {
+
+    }
+}
+
+abstract class Animal{
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    //这里eat实现了，但其实没什么意义
+    //即父类方法不确定性
+    //===> 考虑将该方法设计为抽象(abstract)方法
+    //===> 所谓抽象方法就是没有实现的方法
+    //===> 所谓没有实现就是指，没有方法体
+    //===> 当一个类中存在抽象方法时，需要将该类声明为 abstract 类
+    //===> 一般来说，抽象类会被继承，有其子类来实现抽象方法.
+    public abstract void eat();
+//    {
+//        System.out.println("这是一个动物，但不知道吃啥...");
+//    }
+}
