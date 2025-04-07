a0e096a6543625288289ec590ff5c336fc538445
HHZZY666 <hhzzy666@gmail.com>
2025-04-07 19:57:10 +0800

    Add files via upload

---
+++ chapter10/src/com/hzyedu/interface_/OracleDB.java
@@ -0,0 +1,14 @@
+package com.hzyedu.interface_;
+
+public class OracleDB implements DBInterface{
+
+    @Override
+    public void connect() {
+        System.out.println("连接Oracle");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("关闭Oracle");
+    }
+}
