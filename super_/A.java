869b785c19531c2745cce6ee431e6ccc144413df
HHZZY666 <hhzzy666@gmail.com>
2025-04-08 19:03:56 +0800

    Add files via upload

--- super_/A.java
+++ super_/A.java
@@ -2,7 +2,7 @@ package com.hzyedu.super_;
 
 public class A extends Super01{
     //4 个属性
-    public int n1 = 100;
+    //public int n1 = 100;
     protected int n2 = 200;
     int n3 = 300;
     private int n4 = 400;
--- super_/Super01.java
+++ super_/Super01.java
@@ -1,7 +1,9 @@
 package com.hzyedu.super_;
 
 public class Super01 {
+    public int n1 = 666;
     public static void main(String[] args) {
+        int n1 = 666;
         B b = new B();//子类对象
 //b.sum();
         b.test();
