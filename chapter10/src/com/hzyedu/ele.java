6280180c08ba8062b68c90908cf964f96d6beda1
hhzzy666 <hzy050516@qq.com>
2025-04-10 12:21:28 +0800

    Java idea

--- chapter10/src/com/hzyedu/ele.java
+++ chapter10/src/com/hzyedu/ele.java
@@ -2,6 +2,7 @@ package com.hzyedu;
 
 public class ele {
     public static void main(String[] args) {
-        System.out.println("我好饿！！！");
+        System.out.println("我好饿！！！我要吃饭");
+        //hene.main;
     }
 }
