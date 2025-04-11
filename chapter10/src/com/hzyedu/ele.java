c8c3418f868d6b60b45f792fb56ce8519f9eae40
HHZZY666 <hhzzy666@gmail.com>
2025-04-11 11:30:36 +0800

    Add files via upload

---
+++ chapter10/src/com/hzyedu/ele.java
@@ -0,0 +1,13 @@
+package com.hzyedu;
+
+public class ele {
+    public static void main(String[] args) {
+        System.out.println("我好饿！！！我要吃饭");
+        System.out.println(meituan.class);
+        System.out.println(hene.class);
+    }
+}
+class meituan {
+    public meituan(){
+        System.out.println("确实很饿呜呜呜~");}
+}
