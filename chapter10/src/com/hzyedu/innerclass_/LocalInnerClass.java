b135a170786437bcedf0e40bf6ef52e4a7de2929
hhzzy666 <hzy050516@qq.com>
2025-04-12 15:43:22 +0800

    Java idea

--- chapter10/src/com/hzyedu/innerclass_/LocalInnerClass.java
+++ chapter10/src/com/hzyedu/innerclass_/LocalInnerClass.java
@@ -29,7 +29,7 @@ class Outer02 {//外部类
             public void f1() {
                 //5. 局部内部类可以直接访问外部类的成员，比如下面 外部类 n1 和 m2()
                 //7. 如果外部类和局部内部类的成员重名时，默认遵循就近原则，如果想访问外部类的成员，
-                // 使用 外部类名.this.成员）去访问
+                //   使用（外部类名.this.成员）去访问
                 // 老韩解读 Outer02.this 本质就是外部类的对象, 即哪个对象调用了 m1, Outer02.this 就是哪个对象
                 System.out.println("n1=" + n1 + " 外部类的 n1=" + Outer02.this.n1);
                 System.out.println("Outer02.this hashcode=" + Outer02.this);
