692d22eba879d392d34d4f3ff91fe8bca90c3f4b
hhzzy666 <hzy050516@qq.com>
2025-04-13 14:43:40 +0800

    Java idea

--- chapter10/src/com/hzyedu/innerclass_/InnerClassExercise02.java
+++ chapter10/src/com/hzyedu/innerclass_/InnerClassExercise02.java
@@ -4,6 +4,15 @@ public class InnerClassExercise02 {
     public static void main(String[] args) {
 
         Cellphone cellphone = new Cellphone();
+        //解读：
+        //1.传递的是实现了 Bell 接口的匿名内部类
+        //2.重写了 ring
+        //3.Bell bell = new Bell(){
+        //           @Override
+        //           public void ring () {
+        //              System.out.println("懒猪起床了");
+        //           }
+        //       }
         cellphone.alarmClock(new Bell() {
             @Override
             public void ring() {
@@ -26,6 +35,7 @@ interface Bell{ //接口
 
 class Cellphone { //类
     public void alarmClock(Bell bell){ //形参是接口类型
-        bell.ring();
+        System.out.println(bell.getClass());
+        bell.ring(); //动态绑定
     }
 }
