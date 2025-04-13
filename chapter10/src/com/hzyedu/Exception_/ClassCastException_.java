0c4c7b352ae4738efa1916395d8fd5446811a6e5
HHZZY666 <hhzzy666@gmail.com>
2025-04-13 21:39:57 +0800

    Add files via upload

---
+++ chapter10/src/com/hzyedu/Exception_/ClassCastException_.java
@@ -0,0 +1,12 @@
+package com.hzyedu.Exception_;
+
+public class ClassCastException_ {
+    public static void main(String[] args) {
+        A b = new B(); //向上转型
+        B b2 = (B)b;//向下转型，这里是 OK
+        C c2 = (C)b;//这里抛出 ClassCastException
+    }
+}
+class A {}
+class B extends A {}
+class C extends A {}
