4196d2ac132ba00e6b91a40ca96a4b9ccdfb7d86
hhzzy666 <hzy050516@qq.com>
2025-04-15 16:04:17 +0800

    Java idea

--- chapter12/src/src/com/hzyedu_/try_/TryCatchDetail.java
+++ chapter12/src/src/com/hzyedu_/try_/TryCatchDetail.java
@@ -8,10 +8,12 @@ public class TryCatchDetail {
         //2. 如果异常没有发生，则顺序执行 try 的代码块，不会进入到 catch
         //3. 如果希望不管是否发生异常，都执行某段代码(比如关闭连接，释放资源等)则使用如下代码- finally
         try {
-            //String str = "123";
-            String str = "韩顺平";
-            int a = Integer.parseInt(str);
-            System.out.println("数字：" + a);
+            String str1 = "123";
+            String str2 = "韩顺平";
+            int a1 = Integer.parseInt(str1);
+            System.out.println("数字：" + a1);
+            int a2 = Integer.parseInt(str2);
+            System.out.println("数字：" + a2);
         } catch (NumberFormatException e) {
             System.out.println("异常信息 = " + e.getMessage());
         }finally {
