038e7526e2a565d17fcbe5e4767d57220d774dd0
hhzzy666 <hzy050516@qq.com>
2025-04-15 18:26:11 +0800

    Java idea

--- chapter12/src/src/com/hzyedu_/try_/TryCatchDetail03.java
+++ chapter12/src/src/com/hzyedu_/try_/TryCatchDetail03.java
@@ -1,4 +1,16 @@
 package src.com.hzyedu_.try_;
 
 public class TryCatchDetail03 {
+    public static void main(String[] args) {
+
+        try{
+            int n1 = 10;
+            //int n2 = 2;
+            int n2 = 0;
+            System.out.println(n1 / n2);
+        }finally {
+            System.out.println("执行了finally...");
+        }
+        System.out.println("程序继续执行...");
+    }
 }
