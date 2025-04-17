3835a9c1d707b935e97079262324057d46c7118c
hhzzy666 <hzy050516@qq.com>
2025-04-17 21:06:10 +0800

    Java idea

---
+++ chapter14/src/com/hzyedu/collection_/CollectionExercise.java
@@ -0,0 +1,12 @@
+package com.hzyedu.collection_;
+
+public class CollectionExercise {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Dog{
+    private String name;
+    private int age;
+}
