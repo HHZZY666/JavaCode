5521e928e0d3f5d981129be66a1e26dace1cf663
hhzzy666 <hzy050516@qq.com>
2025-04-17 12:38:23 +0800

    Java idea

---
+++ chapter13/src/com/hzyedu/wrapper_/WrapperType.java
@@ -0,0 +1,15 @@
+package com.hzyedu.wrapper_;
+
+public class WrapperType {
+    public static void main(String[] args) {
+
+        //boolean -> Boolean
+        //char -> Character
+        //byte -> Byte
+        //short -> Short
+        //int -> Integer
+        //long -> Long
+        //float -> Float
+        //double -> Double
+    }
+}
