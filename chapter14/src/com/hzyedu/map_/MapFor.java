3b3366891f85a8903b8be3dc191c58196b5fbdeb
HHZZY666 <hhzzy666@gmail.com>
2025-04-20 15:00:50 +0800

    Add files via upload

---
+++ chapter14/src/com/hzyedu/map_/MapFor.java
@@ -0,0 +1,71 @@
+package com.hzyedu.map_;
+
+import java.util.*;
+
+@SuppressWarnings("all")
+public class MapFor {
+    public static void main(String[] args) {
+
+        Map map = new HashMap();
+        map.put("邓超", "孙俪");
+        map.put("王宝强", "马蓉");
+        map.put("宋喆", "马蓉");
+        map.put("刘令博", null);
+        map.put(null, "刘亦菲");
+        map.put("鹿晗", "关晓彤");
+
+        //第一组：先取出所有Key，通过Key取出对应的Value
+        Set keySet = map.keySet();
+        //(1)增强for
+        System.out.println("=====增强for=====");
+        for (Object key : keySet) {
+            System.out.println(key + "-" + map.get(key));
+        }
+        //(2)迭代器
+        System.out.println("======迭代器======");
+        Iterator iterator = keySet.iterator();
+        while (iterator.hasNext()) {
+            Object key = iterator.next();
+            System.out.println(key + "-" + map.get(key));
+        }
+
+        //第二组：把所有values取出
+        Collection values = map.values();
+        //这里可以使用所有Collections使用的遍历方法
+        //（1）增强for
+        System.out.println("===取出所有的values===");
+        System.out.println("=====增强for=====");
+        for (Object value : values) {
+            System.out.println(value);
+        }
+        //（2）迭代器
+        System.out.println("======迭代器======");
+        Iterator iterator1 = values.iterator();
+        while (iterator1.hasNext()) {
+            Object value = iterator1.next();
+            System.out.println(value);
+        }
+
+        //第三组：通过EntrySet来获取 k-v
+        Set entrySet = map.entrySet();
+        //(1)增强for
+        System.out.println("=====使用EnrtySet中的增强for=====");
+        for (Object enrty : entrySet) {
+            //将entry转成Map.Entry
+            Map.Entry m = (Map.Entry)enrty;
+            System.out.println(m.getKey() + "-" + m.getValue());
+        }
+        //(2)迭代器
+        System.out.println("======使用EntrySet中的迭代器======");
+        Iterator iterator2 = entrySet.iterator();
+        while (iterator2.hasNext()) {
+            Object entry = iterator2.next();
+            //System.out.println(entry);
+            //System.out.println(entry.getClass());//HashMap$Node -实现-> Map.Entry (getKey,getValue)
+            //向下转型
+            Map.Entry m = (Map.Entry) entry;
+            System.out.println(m.getKey() + "-" + m.getValue());
+        }
+
+    }
+}
