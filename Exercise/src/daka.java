d0826c322e4b069a188f059191d918af0aea350a
HHZZY666 <hhzzy666@gmail.com>
2025-04-25 21:35:00 +0800

    Add files via upload

---
+++ Exercise/src/daka.java
@@ -0,0 +1,5 @@
+public class daka {
+    public static void main(String[] args) {
+        System.out.println("五一打卡...");
+    }
+}
---
+++ Exercise/src/ele.java
@@ -0,0 +1,5 @@
+public class ele {
+    public static void main(String[] args) {
+        System.out.println("打卡");
+    }
+}
